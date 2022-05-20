77f5f7e3e7d29dadc63a6fcd9ea107359654de96
Aviv Kalmanson <avivkalmanson@gmail.com>
2022-05-20 18:12:21 +0300

    ext

--- server.js
+++ server.js
@@ -117,22 +117,39 @@ const connect = (ssid, password, countryCode = COUNTRY) => {
   );
   fs.writeFileSync("/etc/wpa_supplicant/wpa_supplicant.conf", fileContent);
 
-  cp.exec("sudo killall wpa_supplicant");
-  cp.exec(
-    `sudo wpa_supplicant -B -i${IFFACE_CLIENT} -c /etc/wpa_supplicant/wpa_supplicant.conf`
-  );
+  cp.exec("sudo killall wpa_supplicant", () => {
+    cp.exec(
+      `sudo wpa_supplicant -B -i${IFFACE_CLIENT} -c /etc/wpa_supplicant/wpa_supplicant.conf`,
+      () => {
+        cp.exec(`sudo wpa_cli -i${IFFACE_CLIENT} RECONFIGURE`, () => {
+          cp.exec(`sudo ifconfig ${IFFACE_CLIENT} up`, () => {
+            cp.exec("sudo systemctl daemon-reload", () => {
+              cp.exec("sudo systemctl restart dhcpcd", () => {
+                if (!checkIfIsConnected()) {
+                  console.log("failed to connect");
+                } else {
+                  disableAccessPoint();
+                  console.log("connected");
+                }
+              });
+            });
+          });
+        });
+      }
+    );
+  });
 
-  cp.execSync(`sudo wpa_cli -i${IFFACE_CLIENT} RECONFIGURE`);
-  cp.execSync(`sudo ifconfig ${IFFACE_CLIENT} up`);
-  cp.execSync("sudo systemctl daemon-reload");
-  cp.execSync("sudo systemctl restart dhcpcd");
+  // cp.execSync(`sudo wpa_cli -i${IFFACE_CLIENT} RECONFIGURE`);
+  // cp.execSync(`sudo ifconfig ${IFFACE_CLIENT} up`);
+  // cp.execSync("sudo systemctl daemon-reload");
+  // cp.execSync("sudo systemctl restart dhcpcd");
 
-  if (!checkIfIsConnected()) {
-    console.log("failed to connect");
-  } else {
-    disableAccessPoint();
-    console.log("connected");
-  }
+  // if (!checkIfIsConnected()) {
+  //   console.log("failed to connect");
+  // } else {
+  //   disableAccessPoint();
+  //   console.log("connected");
+  // }
 
   // cp.exec(`sudo reboot`);
 };
